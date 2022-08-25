5afcb7ca452770200ce63ec2010b8bc9b014276b
OK <@mail.ru>
2022-08-25 12:08:05 +0300

    add 001

---
+++ 001/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+class Program
+{
+    static void FillArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine("Введите  числo" );
+
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        Console.Write("[");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i == arr.Length - 1)
+            {
+                Console.Write(arr[i] + "");
+            }
+            else
+            {
+                Console.Write(arr[i] + ", ");
+            }
+        }
+        Console.Write("]");
+
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество элементов массива ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[n];
+        FillArray(array);
+        Console.WriteLine("Массив");
+        PrintArray(array);
+        Console.WriteLine();
+        Console.WriteLine("Количество чисел больше 0 ");
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+            if (array[i] > 0)
+            {
+                count++;
+            }
+        Console.WriteLine($"{count}");
+    }
+}
+
+
+
+
+
