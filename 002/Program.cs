935b95ef4fd622fa871e982920a6971c53a07b63
OK <@mail.ru>
2022-08-26 17:46:29 +0300

    add 002

--- 002/Program.cs
+++ 002/Program.cs
@@ -11,15 +11,19 @@ class Program
         double k1 = Convert.ToInt32(Console.ReadLine());
         double b2 = Convert.ToInt32(Console.ReadLine());
         double k2 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Точка пересечения двух прямых ");
         double x = (b2 - b1) / (k1 - k2);
         double y = k1 * x + b1;
-        if (k1 == k2)
+        if ((k1 == k2) & !(b1 == b2))
         {
             Console.WriteLine("Прямые параллельны");
         }
-        else
+        if (k1 == k2 & b1 == b2)
         {
+            Console.WriteLine("Прямые совпадают");
+        }
+        if (!(k1 == k2))
+        {
+            Console.WriteLine("Точка пересечения двух прямых ");
             Console.WriteLine($"({x}, {y})");
         }
     }
